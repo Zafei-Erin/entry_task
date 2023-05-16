f905346e47c10f38f2d3b389acd3c3da5de3897a
Zafei-Erin <897659752@qq.com>
2023-05-16 11:10:57 +0800

    use Suspense and createresource for states

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -10,7 +10,7 @@ export default function ResultCard(props: PropsType) {
     const result = {
         name: props.rawResult.name,
         symbol: props.rawResult.symbol,
-        totalSupply: (Number(props.rawResult.total_supply)/(Math.pow(10, props.rawResult.decimals))).toFixed(2)
+        totalSupply: (Number(props.rawResult.total_supply)/(Math.pow(10, props.rawResult.decimals))).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
     }
     
 
@@ -19,7 +19,7 @@ export default function ResultCard(props: PropsType) {
             <span class="text-gray-50 font-bold text-xl sm:text-2xl md:text-3xl">{result.name}</span>
             <div class="mt-4 sm:text-lg md:text-xl">
                 <h1 class="text-slate-400 my-1">Symbol: <span class="ml-1 text-gray-50 font-semibold">{result.symbol}</span> </h1>
-                <h1 class="text-slate-400 my-1">Total supply: <span class="ml-1 text-gray-50 font-semibold">{Number(result.totalSupply).toLocaleString()}</span> </h1>
+                <h1 class="text-slate-400 my-1">Total supply: <span class="ml-1 text-gray-50 font-semibold">{result.totalSupply}</span> </h1>
             </div>
 
         </div>
