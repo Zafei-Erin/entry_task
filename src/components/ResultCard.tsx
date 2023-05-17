ef42ee937feef0fe0e139daf08b546f926146db9
aidanaden <aidan@u.nus.edu>
2023-05-17 12:45:53 +0800

    feat: display all access modes

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -21,27 +21,43 @@ export default function ResultCard(props: PropsType) {
     console.log(result)
 
     return(
+        <>
         <div class="w-full mt-6 py-3 px-4 sm:py-4 sm:px-5 bg-gray-800 rounded-lg">
             <span class="text-gray-50 font-bold text-xl sm:text-2xl md:text-3xl">{result.name}</span>
             <div class="mt-4 sm:text-lg md:text-xl">
                 {/* using a memo tracks changes from setInterval in the App.tsx file, UNCOMMENT BELOW TO DEMO */}
-                <h1 class="text-slate-400 my-1">Symbol: ({bigData()})
+                <h1 class="text-slate-400 my-1">Symbol: accessing via memo ({bigData()})
                     <span class="ml-1 text-gray-50 font-semibold">{result.symbol}</span> 
                 </h1>
-
-                {/* using prop attribute directly tracks changes also, since props are signals, UNCOMMENT BELOW TO DEMO */}
-                {/* <h1 class="text-slate-400 my-1">Symbol: ({props.data})
-                    <span class="ml-1 text-gray-50 font-semibold">{result.symbol}</span> 
-                </h1> */}
-
-                {/* accessing prop value by first storing it in an object breaks reactivity, cant track changes, UNCOMMENT BELOW TO DEMO */}
-                {/* <h1 class="text-slate-400 my-1">Symbol: ({result.data})
-                    <span class="ml-1 text-gray-50 font-semibold">{result.symbol}</span> 
-                </h1> */}
                 <h1 class="text-slate-400 my-1">Total supply: 
                     <span class="ml-1 text-gray-50 font-semibold">{result.totalSupply}</span> 
                 </h1>
             </div>
         </div>
+        <div class="w-full mt-6 py-3 px-4 sm:py-4 sm:px-5 bg-gray-800 rounded-lg">
+            <span class="text-gray-50 font-bold text-xl sm:text-2xl md:text-3xl">{result.name}</span>
+            <div class="mt-4 sm:text-lg md:text-xl">
+                {/* using prop attribute directly tracks changes also, since props are signals, UNCOMMENT BELOW TO DEMO */}
+                <h1 class="text-slate-400 my-1">Symbol: accessing via props ({props.data})
+                    <span class="ml-1 text-gray-50 font-semibold">{result.symbol}</span> 
+                </h1>
+                <h1 class="text-slate-400 my-1">Total supply: 
+                    <span class="ml-1 text-gray-50 font-semibold">{result.totalSupply}</span> 
+                </h1>
+            </div>
+        </div>
+        <div class="w-full mt-6 py-3 px-4 sm:py-4 sm:px-5 bg-gray-800 rounded-lg">
+            <span class="text-gray-50 font-bold text-xl sm:text-2xl md:text-3xl">{result.name}</span>
+            <div class="mt-4 sm:text-lg md:text-xl">
+                {/* accessing prop value by first storing it in an object breaks reactivity, cant track changes, UNCOMMENT BELOW TO DEMO */}
+                <h1 class="text-slate-400 my-1">Symbol: accessing via object ({result.data})
+                    <span class="ml-1 text-gray-50 font-semibold">{result.symbol}</span> 
+                </h1>
+                <h1 class="text-slate-400 my-1">Total supply: 
+                    <span class="ml-1 text-gray-50 font-semibold">{result.totalSupply}</span> 
+                </h1>
+            </div>
+        </div>
+        </>
     )
 }
